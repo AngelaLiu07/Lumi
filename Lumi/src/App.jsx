8079a1be2c250798c0249700b60010eb49dc7839
Angela <al2679@cornell.edu>
2026-08-30 15:38:39 -0400

    Foundational Tauri application set up

---
+++ Lumi/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+
+  return (
+    <main className="container">
+      <h1>
+        hello mochi °❀.ೃ࿔*
+      </h1>
+      <button class="welcome-btn">
+        let's get started!
+      </button>
+    </main>
+  );
+}
+
+export default App;
