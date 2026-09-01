226cfbc239235901df1729bb05ded89d0465f392
Angela <al2679@cornell.edu>
2026-08-31 23:37:51 -0400

    Transparent window and draggable character

--- Lumi/src-tauri/src/lib.rs
+++ Lumi/src-tauri/src/lib.rs
@@ -11,4 +11,6 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+
 }
--- Lumi/src/App.jsx
+++ Lumi/src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import mochi from "/Users/angelaliu/Desktop/Lumi/Lumi/src-tauri/icons/clipart3904090.png";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-
   return (
     <main className="container">
-      <h1>
+      <h1 data-tauri-drag-region>
         hello mochi °❀.ೃ࿔*
       </h1>
-      <button class="welcome-btn">
+      <button class="welcome-btn" data-tauri-drag-region>
         let's get started!
       </button>
+      <div data-tauri-drag-region>
+        <img src={mochi} width="100" height="100" class = "center-img" data-tauri-drag-region></img>
+      </div>
     </main>
   );
 }
