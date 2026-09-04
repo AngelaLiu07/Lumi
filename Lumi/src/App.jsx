2075371d31ad9f1cb45b3962f14680f6fc85df99
Angela <al2679@cornell.edu>
2026-09-03 23:17:56 -0400

    added Lumi's icon to the screen, created from Figma

--- Lumi/src/App.jsx
+++ Lumi/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import mochi from "/Users/angelaliu/Desktop/Lumi/Lumi/src-tauri/icons/clipart3904090.png";
+import mochi_icon from "/Users/angelaliu/Desktop/Lumi/Lumi/src-tauri/icons/mochi.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import Mochi from "./Mochi"
@@ -15,7 +15,7 @@ function App() {
         let's get started!
       </button>
       <div data-tauri-drag-region>
-        <img src={mochi} width="100" height="100" class = "center-img" data-tauri-drag-region></img>
+        <img src={mochi_icon} width="100" height="100" class = "center-img" data-tauri-drag-region></img>
       </div>
     </main>
   );
